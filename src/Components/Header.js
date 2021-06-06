5e5bc8fef344bd746346e04db70a7da10db757d7
6110406147 <phitchayatida.c@ku.th>
2021-06-06 23:47:58 +0700

    update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <div>
             <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+            <Navbar.Brand href="#">Todo-list</Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
                 <Nav
@@ -14,16 +14,8 @@ export default function Header() {
                 navbarScroll
                 >
                 <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/post">Link</Nav.Link>
-                <NavDropdown title="Link" id="navbarScrollingDropdown">
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link href="#" disabled>
-                    Link
-                </Nav.Link>
+                <Nav.Link href="/post">Post</Nav.Link>
+                
                 </Nav>
                 <Form className="d-flex">
                 <FormControl
