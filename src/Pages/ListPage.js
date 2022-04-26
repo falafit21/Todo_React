fead3f02586fe84c7c81834ae4d11d079cf34b44
Phitchayatida <phitchayatida.c@ku.th>
2022-04-26 19:19:27 +0700

    test react

--- src/Pages/ListPage.js
+++ src/Pages/ListPage.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import {Container,Table} from 'react-bootstrap'
 export default function ListPage() {
     return (
         <div>
             <Container>
+            <Table striped bordered hover size="sm">
+                        <thead>
+                            <tr>
+                            <th>#</th>
+                            <th>Activities</th>
+                            <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                            {
+                                data.map((key,index)=>{
+                                    return(
+                                        <tr>
+                                            <td>{key.number}</td>
+                                            <td>{key.todo}</td>
+                                            <td>{key.date}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                            
+                        </tbody>
+                    </Table>
 
             </Container>
             List
